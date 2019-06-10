c933ae3e2bd36bbc07c3b3924bed060c79f69055
Kelliann Lafferty <kell10192@gmail.com>
2019-06-10 18:09:32 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,15 @@
+def my_collect(array)
+  i = 0
+  new_array = []
+  while i < array.length
+    new_array << yield(array[i])
+    i = i + 1 
+  end
+  new_array 
+end
 
-
+# can handle an empty collection
+# yields the correct element from a given collection 
+# does no modify the original collection
+# yields the correct element from a given collection 
+# returns a new collection of appropriately modified elements
